6efa3ca1ffac0e3ceaf1282f998a0480371cb858
PickleNickle <softie04@gmail.com>
2023-11-24 21:36:15 +0100

    yes

--- src/main/java/net/senna/tutorialmod/TutorialMod.java
+++ src/main/java/net/senna/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.senna.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.senna.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
